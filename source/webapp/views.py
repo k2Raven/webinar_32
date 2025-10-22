53f4a6f5c720d8ba2395207ba6a4e4577dfa50f9
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-10-22 19:34:30 +0600

    create task1 view

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -17,6 +17,9 @@ def index_view(request):
 def about_view(request):
     return render(request, 'about.html')
 
+def task1_view(request):
+    return render(request, 'task1.html')
+
 def task3_view(request):
     return render(request, 'task3.html')
 
